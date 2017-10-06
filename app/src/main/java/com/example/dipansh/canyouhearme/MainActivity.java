e337625f38661b527535b25e1163d854af376a31
DipanshKhandelwal <deepanshrocks438@gmail.com>
2017-10-07 00:40:21 +0530

    Removed title bar

--- app/src/main/java/com/example/dipansh/canyouhearme/MainActivity.java
+++ app/src/main/java/com/example/dipansh/canyouhearme/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.TextView;
 
 import java.util.ArrayList;
